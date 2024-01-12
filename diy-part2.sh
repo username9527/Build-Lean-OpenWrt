e35dfc8eb0daf7d49df76b3678e2ba9878d4e41c
Githun <imimi2008@gmail.com>
2024-01-12 23:25:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,25 @@ sed -i 's/tplink-4mlzma/tplink-8mlzma/' target/linux/ath79/image/tiny-tp-link.mk
 sed -i 's/tplink-4m/tplink-8m/' target/linux/ath79/image/tiny-tp-link.mk
 
 # Modify default theme
-rm -rf package/lean/luci-theme-argon
-# git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+rm -rf package/feeds/luci/luci-theme-argon
+rm -rf package/feeds/luci/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/custom/luci-app-argon-config
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
 # Add Packages
 # git clone https://github.com/MapesxGM/luci-app-tencentddns package/luci-app-tencentddns
-# rm -rf package/lean/luci-app-easymesh
-# git clone https://github.com/ntlf9t/luci-app-easymesh.git package/lean/luci-app-easymesh
+# git clone -b Lede --single-branch https://github.com/shidahuilang/openwrt-package package/shidahuilang
+# rm -rf package/feeds/luci/luci-app-easymesh
+# git clone -b master --single-branch https://github.com/ntlf9t/luci-app-easymesh package/custom/luci-app-easymesh
+git clone -b main --single-branch https://github.com/username9527/openwrt-package package/custom
+git clone -b main --single-branch https://github.com/siwind/luci-app-wolplus package/custom/luci-app-wolplus
+git clone -b main --single-branch https://github.com/sirpdboy/luci-app-netwizard package/custom/luci-app-netwizard
+git clone -b main --single-branch https://github.com/sirpdboy/luci-app-advancedplus package/custom/luci-app-advancedplus
+git clone -b master --single-branch https://github.com/sirpdboy/luci-app-advanced package/custom/luci-app-advanced
+git clone -b master --single-branch https://github.com/sirpdboy/luci-app-autotimeset package/custom/luci-app-autotimeset
+git clone -b master --single-branch https://github.com/destan19/OpenAppFilter package/custom/OpenAppFilter
+rm -rf package/feeds/luci/luci-app-unblockmusic
+git clone -b master --single-branch https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/custom/luci-app-unblockneteasemusic
+
+
